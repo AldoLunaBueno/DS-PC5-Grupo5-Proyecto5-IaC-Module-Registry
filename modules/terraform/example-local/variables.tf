fb06149044e30ceac22d943f846cdf1bcaf38ccf
Jharvy <jharvy.cadillo.t@uni.pe>
2025-12-07 18:31:45 -0500

    feat: agregar el modulo ejemplo de terraform local

---
+++ modules/terraform/example-local/variables.tf
@@ -0,0 +1,11 @@
+variable "contenido" {
+  description = "El texto que irá dentro del archivo"
+  type        = string
+  default     = "Hola mundo, esta es la Practica 05 de Desarrollo de Software"
+}
+
+variable "nombre_archivo" {
+  description = "Nombre del archivo a generar"
+  type        = string
+  default     = "mensaje.txt"
+}
